6cce61badc6611189b9b0d733539f1d1cb4666b7
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2025-07-01 09:26:25 -0700

    commit changes to directory structure

--- release/f11/plotlims.m
+++
@@ -1,6 +0,0 @@
-function plotlims(x,y, tx,ty, t0,t1, varargin)
-% plot (x, y) with tx,ty in the interval [t0, t1]
-    yy = ( (t0 <= ty) & (ty <= t1) ); % indexes y
-    xx = ( (t0 <= tx) & (tx <= t1) ); % indexes x
-    plot(x(xx), y(yy), varargin{:});
-end
--- release/f12/mass_mixing_test.m
+++
@@ -1,9 +0,0 @@
-% Valid for 2-member mixing %
-function [fw1,fw2] = mass_mixing_test(ind,fev,fss,q_ev,q_surf)    
-    fa1 = fev(ind);
-    fa2 = fss(ind);
-    q1 = q_ev(ind);
-    q2 = q_surf(ind);
-    q = fa1.*q1 + fa2.*q2;
-    fw1 = (fa1.*q1)./q;
-    fw2 = (fa2.*q2)./q;
