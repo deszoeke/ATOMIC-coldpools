f360aa915dff80c47ecadfc63f3aff69720c1ffd
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2025-07-01 14:25:24 -0700

    update documentation, prepend licenses

--- release/figure/f02/P3dataFlagVerticalProfiles.m
+++ release/figure/f02/P3dataFlagVerticalProfiles.m
@@ -1,3 +1,26 @@
+% Copyright 2025 Simon P. de Szoeke and Estefanía Quiñones 
+% Meléndez.
+% 
+% Permission is hereby granted, free of charge, to any person 
+% obtaining a copy of this software and associated documentation 
+% files (the “Software”), to deal in the Software without 
+% restriction, including without limitation the rights to use, 
+% copy, modify, merge, publish, distribute, sublicense, and/or 
+% sell copies of the Software, and to permit persons to whom the 
+% Software is furnished to do so, subject to the following 
+% conditions:
+%
+% The above copyright notice and this permission notice shall be 
+% included in all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+% OTHER DEALINGS IN THE SOFTWARE.
 %% pdf from P3 data
 % addpath 'D:\BackUp_Personal_Laptop_March2022\Research Year 2020-2021\Recovery_PersonalLaptop_09022020\OneDrive\Documents\ATOMIC_Files\RHB raw files\Dean''s P3 files with flags'
 % addpath 'C:\Users\quinones\Documents\Data\thermo'
--- release/figure/f11/plotqd.m
+++ release/figure/f11/plotqd.m
@@ -1,3 +1,26 @@
+% Copyright 2025 Simon P. de Szoeke and Estefanía Quiñones 
+% Meléndez.
+% 
+% Permission is hereby granted, free of charge, to any person 
+% obtaining a copy of this software and associated documentation 
+% files (the “Software”), to deal in the Software without 
+% restriction, including without limitation the rights to use, 
+% copy, modify, merge, publish, distribute, sublicense, and/or 
+% sell copies of the Software, and to permit persons to whom the 
+% Software is furnished to do so, subject to the following 
+% conditions:
+%
+% The above copyright notice and this permission notice shall be 
+% included in all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+% OTHER DEALINGS IN THE SOFTWARE.
 function plotqd(q_surf_b, qi_surf, q_ent_b, qi_ent, n)
 % plotqd(q0,qi0, q1,qi1, n)
 % q - deltaD plot mixing line between end members 0,1.
