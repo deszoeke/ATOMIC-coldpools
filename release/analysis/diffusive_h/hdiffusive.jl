d149a773d75d195982764e699502f23e924c5adb
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2026-03-18 22:21:50 -0700

    calc hdiffusive.jl

--- release/analysis/diffusive_h/hdiffusive.jl
+++ release/analysis/diffusive_h/hdiffusive.jl
@@ -1,12 +1,13 @@
-using Pkg. Pkg.activate(".")
+using Pkg; Pkg.activate(".")
 
 using NCDatasets
 using CSV, DataFrames
 using Dates
 using Statistics
+using PyPlot
 
-ds = NCDatasets("data/EUREC4A_ATOMIC_RonBrown_10min_nav_met_sea_flux_20200109-20200212_v1.3.nc")
-ds[:ustar]
+ds = NCDataset("data/EUREC4A_ATOMIC_RonBrown_10min_nav_met_sea_flux_20200109-20200212_v1.3.nc")
+# ds[:ustar]
 psltime = DateTime(2020,1,1) .+ Second.(round.(ds[:time][:]))
 
 tb = CSV.read("table2.txt", DataFrame, delim=' ', header=false, ignorerepeated=true)
@@ -20,3 +21,10 @@ idx = [searchsortedlast(psltime, cpt) for cpt in cptime]
 ustar = [mean(ds[:ustar][i:i+5]) for i in idx]
 
 hD = kappa * ustar ./ lambdai
+hi = tb[!,end]
+
+plot(hi, hD, marker="o", linestyle="none")
+plot([0,150],[0,150],"k-")
+plot([0,150],[0,5*150],"k-")
+xlabel(L"h_i"); ylabel(L"h_D")
+
